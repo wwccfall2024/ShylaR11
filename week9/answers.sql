ec7d3a7984bb48beff9d6b2cbc2355d42b89268a
ShylaR11 <shyla.r.ayala@student.wwcc.edu>
2024-12-03 21:26:24 -0800

    Update users

--- week9/answers.sql
+++ week9/answers.sql
@@ -1,3 +1,10 @@
 -- Create your tables, views, functions and procedures here!
 CREATE SCHEMA social;
 USE social;
+CREATE TABLE user (
+  user_id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_on DATETIME DEFAULT CURRENT_TIMESTAMP
+  );
